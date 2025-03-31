e092fee509c51108c57e87fbb66161b44dcdef3e
Arkadiy Ivanov <digitmir@gmail.com>
2025-03-31 17:10:54 +0300

    remove some categories

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,19 +1,19 @@
 
 export const navigation = [
     { name: 'Кухни', href: '/kuhni' },
-    { name: 'Столы и стулья', href: '/stoli_i_stulya' },
+    // { name: 'Столы и стулья', href: '/stoli_i_stulya' },
     { name: 'Гардеробные', href: '/garderobnie' },
     { name: 'Гостинные', href: '/gostinnie' },
     { name: 'Прихожие', href: '/prihojie' },
     { name: 'Спальни', href: '/spalni' },
-    { name: 'Шкафы купе', href: '/shkafi_kupe' },
+    // { name: 'Шкафы купе', href: '/shkafi_kupe' },
     { name: 'Шкафы распашные', href: '/shkafi_raspashnie' },
     { name: 'Кровати', href: '/krovati'},
-    { name: 'Матрасы', href: '/matraci' },
+    // { name: 'Матрасы', href: '/matraci' },
     { name: 'Детские', href: '/detskie' },
-    { name: 'Санузлы', href: '/sanuzli' },
-    { name: 'Рабочие зоны', href: '/rabochie_zoni' },
-    { name: 'Декоративные панели', href: '/decorativnie_paneli' },
+    // { name: 'Санузлы', href: '/sanuzli' },
+    // { name: 'Рабочие зоны', href: '/rabochie_zoni' },
+    // { name: 'Декоративные панели', href: '/decorativnie_paneli' },
 ];
 
 
