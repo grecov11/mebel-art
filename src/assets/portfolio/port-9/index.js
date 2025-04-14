eb05fcf4992a5066826d4e421974bdfcd13584c1
Arkadiy Ivanov <digitmir@gmail.com>
2025-04-14 17:43:00 +0300

    add img to portfolio

---
+++ src/assets/portfolio/port-9/index.js
@@ -0,0 +1,12 @@
+import img1 from "./img1.webp"
+import img2 from "./img2.webp"
+import img3 from "./img3.webp"
+import img4 from "./img4.webp"
+
+
+export const port9 = [
+    {img: img1},
+    {img: img2},
+    {img: img3},
+    {img: img4},
+]
